1205ac1efc732b292b7b35a8d5701a6b6e593a48
MacMini - 24 <macmini-24@MacMinis-Mac-mini.local>
2023-04-04 14:34:42 -0700

    initial commit

---
+++ test/specs/key_presses.spec.js
@@ -0,0 +1,13 @@
+// const KeyPressesPage = require('../pageobjects/key_press.page');
+import KeyPressesPage from '../pageobjects/key_press.page'
+
+describe('My Key Press application', () => {
+    it('should display what is entered into the input', async () => {
+        await KeyPressesPage.open();
+
+        // await KeyPressesPage.pressAkey('C');
+
+        // await expect(KeyPressesPage.keyPressResult).toHaveTextContaining(
+        //     'You entered: C');
+    });
+});
